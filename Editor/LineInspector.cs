d940c1a9c2e9aacb298c4d9ec2970e2aa3e1ad86
kp16 <kap.96@live.com>
2017-01-31 21:40:54 +0000

    Added files

---
+++ Editor/LineInspector.cs
@@ -0,0 +1,42 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Line))]
+public class LineInspector : Editor
+{
+    private void OnSceneGUI()
+    {
+        // drawing the lines n stuff in world space
+        Line line = target as Line;
+        Transform handleTransform = line.transform;
+        Quaternion handleRotation = Tools.pivotRotation == PivotRotation.Local ?
+            handleTransform.rotation : Quaternion.identity;
+        //handleTransform is the transform from handle to line
+        //call these functions to transform points of line in ambient space
+        Vector3 p0 = handleTransform.TransformPoint(line.p0);
+        Vector3 p1 = handleTransform.TransformPoint(line.p1);
+
+        Handles.color = Color.white;
+        Handles.DrawLine(line.p0, line.p1);
+        Handles.DoPositionHandle(p0, handleRotation);
+        Handles.DoPositionHandle(p1, handleRotation);
+
+        EditorGUI.BeginChangeCheck();
+        p0 = Handles.DoPositionHandle(p0, handleRotation);
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(line, "Move Point");
+            EditorUtility.SetDirty(line);
+            line.p0 = handleTransform.InverseTransformPoint(p0);
+        }
+        EditorGUI.BeginChangeCheck();
+        p1 = Handles.DoPositionHandle(p1, handleRotation);
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(line, "Move Point");
+            EditorUtility.SetDirty(line);
+            line.p1 = handleTransform.InverseTransformPoint(p1);
+        }
+    }
+
+}
---
+++ Line.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Line : MonoBehaviour
+{
+    public Vector3 p0, p1;
+}
+
